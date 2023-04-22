702b20671bf3739947675d55524e2fc54a8ce20a
Christian <cibucristi1@gmail.com>
2023-04-22 03:46:07 +0300

    added the database

--- database/rage-ts.sql
+++ database/rage-ts.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2023 at 11:59 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Apr 22, 2023 at 02:45 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `rage-ts`
+-- Database: `adrian`
 --
 
 -- --------------------------------------------------------
@@ -38,7 +38,7 @@ CREATE TABLE `accounts` (
   `admin` int(11) NOT NULL,
   `helper` int(11) NOT NULL,
   `respect` int(11) NOT NULL,
-  `level` int(11) NOT NULL,
+  `level` int(11) NOT NULL DEFAULT 1,
   `money` int(11) NOT NULL,
   `bank` int(11) NOT NULL,
   `warns` int(11) NOT NULL,
@@ -56,14 +56,32 @@ CREATE TABLE `accounts` (
   `shirt` int(11) NOT NULL,
   `skill` varchar(24) NOT NULL DEFAULT '0|0',
   `times` varchar(24) NOT NULL DEFAULT '0|0',
-  `driving_license_active` int(11) NOT NULL,
-  `driving_license_suspended` int(11) NOT NULL,
-  `weapon_license_active` int(11) NOT NULL,
-  `weapon_license_suspended` int(11) NOT NULL,
-  `fly_license_active` int(11) NOT NULL,
-  `fly_license_suspended` int(11) NOT NULL,
-  `boat_license_active` int(11) NOT NULL,
-  `boat_license_suspended` int(11) NOT NULL
+  `licenses` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{   "driving_license": {     "status": "inactive",     "expiration_date": 0,     "suspend_hours": 0   },   "weapon_license": {     "status": "inactive",     "expiration_date": 0,     "suspend_hours": 0   },   "flying_license": {     "status": "inactive",     "expiration_date": 0,     "suspend_hours": 0   },   "boat_license": {     "status": "inactive",     "expiration_date": 0,     "suspend_hours": 0   } }'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`ID`, `username`, `password`, `email`, `socialClub`, `socialClubId`, `lastActive`, `admin`, `helper`, `respect`, `level`, `money`, `bank`, `warns`, `mute`, `hours`, `skin`, `seconds`, `job`, `fishrod`, `momeala`, `gender`, `hair`, `pants`, `shoes`, `shirt`, `skill`, `times`, `licenses`) VALUES
+(1, 'cibu', '$2a$10$0zBs.z8Tk7I9DxNPEcIku.1K.bnRrnFlrSX4ENb9pvjRlNZaL5VLW', 'cibucristi1@gmail.com', 'BillyBoyBean', 183677645, 2147483647, 8, 0, 2, 0, 0, 0, 0, 0, 0, 'mp_m_freemode_01', 0, 0, 0, 0, 1, 1, 3, 4, 3, '0|0', '0|0', '{\"driving_license\":{\"status\":\"active\",\"expiration_date\":1682483754,\"suspend_hours\":0},\"weapon_license\":{\"status\":\"active\",\"expiration_date\":1682159052,\"suspend_hours\":0},\"flying_license\":{\"status\":\"active\",\"expiration_date\":1683204163,\"suspend_hours\":0},\"boat_license\":{\"status\":\"inactive\",\"expiration_date\":0,\"suspend_hours\":10}}');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bans`
+--
+
+CREATE TABLE `bans` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `socialClubId` int(11) NOT NULL,
+  `active` int(11) NOT NULL,
+  `permanent` tinyint(1) NOT NULL,
+  `days` int(11) NOT NULL,
+  `reason` varchar(128) NOT NULL,
+  `adminName` varchar(32) NOT NULL,
+  `time` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -88,6 +106,13 @@ CREATE TABLE `business` (
   `status` tinyint(1) NOT NULL,
   `owned` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `business`
+--
+
+INSERT INTO `business` (`ID`, `level`, `price`, `balance`, `type`, `icon`, `icon_color`, `x`, `y`, `z`, `description`, `owner`, `status`, `owned`) VALUES
+(1, 1, 1, 0, 1, 431, 37, -1287.79, -1508.73, 4.33847, 'RAGE.RAYED.MP', 'AdmBot', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -126,24 +151,6 @@ CREATE TABLE `jobs` (
   `description` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `bans`
---
-
-CREATE TABLE `bans` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `socialClubId` int(11) NOT NULL,
-  `active` int(11) NOT NULL,
-  `permanent` tinyint(1) NOT NULL,
-  `days` int(11) NOT NULL,
-  `reason` varchar(128) NOT NULL,
-  `adminName` varchar(32) NOT NULL,
-  `time` datetime DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Indexes for dumped tables
 --
@@ -152,6 +159,12 @@ CREATE TABLE `bans` (
 -- Indexes for table `accounts`
 --
 ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `bans`
+--
+ALTER TABLE `bans`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -167,12 +180,6 @@ ALTER TABLE `jobs`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `bans`
---
-ALTER TABLE `bans`
-  ADD PRIMARY KEY (`ID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -180,24 +187,24 @@ ALTER TABLE `bans`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bans`
+--
+ALTER TABLE `bans`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `business`
 --
 ALTER TABLE `business`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `bans`
---
-ALTER TABLE `bans`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
